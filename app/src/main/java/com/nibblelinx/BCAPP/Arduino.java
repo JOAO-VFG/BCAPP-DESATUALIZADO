b7d8f150072680af9f0df5e50b255c9a44926a61
JOAO-VFG <78110240+JOAO-VFG@users.noreply.github.com>
2022-08-06 18:25:23 -0400

    atualizacao de projeto com SDK 30

--- app/src/main/java/com/nibblelinx/BCAPP/Arduino.java
+++ app/src/main/java/com/nibblelinx/BCAPP/Arduino.java
@@ -1,15 +1,138 @@
 package com.nibblelinx.BCAPP;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class Arduino extends AppCompatActivity {
 
     // Button ButtonMS9Back;
+
+    Button buttonConectarArduino, buttonLED1, buttonLED2, buttonLED3;
+    private static final int SOLICITA_ATIVACAO = 1;
+    private static final int SOLICITA_CONEXAO = 2;
+    BluetoothAdapter meuBluetoothAdapter = null;
+    BluetoothDevice meuDevice = null;
+    BluetoothSocket meuSocket = null;
+
+    boolean conexao = false;
+    UUID meu_UUID =  UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arduino);
 
+        buttonConectarArduino = findViewById(R.id.buttonConectarArduino);
+        buttonLED1 = findViewById(R.id.buttonLED1);
+        buttonLED2 = findViewById(R.id.buttonLED2);
+        buttonLED3 = findViewById(R.id.buttonLED3);
+
+        meuBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        if(meuBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Seu dispositivo não possui bluetooth", Toast.LENGTH_LONG).show();
+        }else if(!meuBluetoothAdapter.isEnabled()) {
+            Intent ativaBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(ativaBluetooth, SOLICITA_ATIVACAO);
+        }
+
+        buttonConectarArduino.setOnClickListener(v -> {
+            if(conexao){
+                //desconectar
+                try{
+                    meuSocket.close();
+                    conexao = false;
+                    buttonConectarArduino.setText("Conectar");
+                    Toast.makeText(getApplicationContext(), "O bluetooth foi desconectado", Toast.LENGTH_LONG).show();
+
+                } catch (IOException erro){
+                    Toast.makeText(getApplicationContext(), "ocorreu um erro: " + erro, Toast.LENGTH_LONG).show();
+
+                }
+            }else{
+                Intent abreLista = new Intent(Arduino.this, ListaDispositivos.class);//conectar
+                startActivityForResult(abreLista, SOLICITA_CONEXAO );
+            }
+        });
     }
-}
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+
+            case SOLICITA_ATIVACAO:
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(getApplicationContext(), "O bluetooth foi ativado", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "O bluetooth não foi ativado, o aplicativo será encerrado", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            case SOLICITA_CONEXAO:
+                if(resultCode == Activity.RESULT_OK){
+                    assert data != null;
+                    String MAC = data.getExtras().getString(ListaDispositivos.ENDERECO_MAC);
+                    Toast.makeText(getApplicationContext(), "MAC Final: " + MAC, Toast.LENGTH_LONG).show();
+                    meuDevice = meuBluetoothAdapter.getRemoteDevice(MAC);
+
+                    try {
+                        meuSocket = meuDevice.createRfcommSocketToServiceRecord(meu_UUID);
+                        meuSocket.connect();
+                        conexao = true;
+                        buttonConectarArduino.setText("Desconectar");
+                        Toast.makeText(getApplicationContext(), "Você foi conectado com: " + MAC, Toast.LENGTH_LONG).show();
+                    } catch (IOException erro) {
+                        conexao = false;
+                        Toast.makeText(getApplicationContext(), "Ocorreu um erro: " + MAC, Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "falha ao obter o MAC", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+    }
